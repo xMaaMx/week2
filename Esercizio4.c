5fe162c1c20eb19c8a92ff5f2c32b0af15cf15c8
Massimiliano Greco <102535941+xMaaMx@users.noreply.github.com>
2022-07-14 11:39:58 +0200

    Add files via upload

--- Esercizio4.c
+++ Esercizio4.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 int main ()
 {
+	/*dichiarazioni variabili*/
 	int valoreuno;
 	int valoredue;
 	float media;
 
+	/*stampa input utente leggendo poi il contenuto*/
 	printf("inserisci numero\n" );
 	scanf("%d",&valoreuno);
 
 	printf("inserisci secondo numero\n");
 	scanf("%d",&valoredue);
 
-	media = (valoreuno + valoredue)/2;
+	media = (valoreuno + valoredue)/2;//esegue la media dei valori
 
-	printf("media di valori: %f" , media);
+	printf("media di valori: %4.0f" , media);//stampa la media
 
 }
