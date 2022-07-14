c5544ccac73c50e84a560538326d89e5d1cf5446
Massimiliano Greco <102535941+xMaaMx@users.noreply.github.com>
2022-07-14 10:14:20 +0200

    Add files via upload

--- Esercizio3.c
+++ Esercizio3.c
@@ -2,16 +2,25 @@
 int main ()
 {
 	int numerouser;//variabile numero utente
-
+	int decremento;
 	printf("inserire un numero  \n" );//richiesta scritta all'utente
 	scanf("%d",&numerouser);//input dall'utente
-	
+
 	printf("%d" ,numerouser);//stampa  risultato input
 	printf("questo è il tuo input\n"   );
 	if (numerouser == numerouser)
 		{
 		numerouser ++;
 		}
+
 	printf("%d il tuo nuovo input:"   ,numerouser);
+	printf("Inserisci nuovo numero \n");
+
+	scanf("%d",&decremento);
+	if(numerouser = numerouser)
+	{
+	decremento -- ;
+	}
+	printf("%d il tuo nuovo punteggio:"   ,decremento);
 
 }
